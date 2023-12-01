3df753086f77177c4525f5ead33346dc275820ec
Qqruzko <150928737+Qqruzko@users.noreply.github.com>
2023-12-01 16:33:46 +0300

    Add files via upload

---
+++ Lab9/Lab11/Distance.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Distance
+    {
+        public int feet { get; set; }
+        public double inches { get; set; }
+        public override string ToString()
+        {
+            return String.Format("{0} - {1}", feet, inches);
+        }
+        public static bool operator !=(Distance A1, Distance A2)
+        {
+            if (A1.feet == A2.feet)
+                return true;
+            return false;
+        }
+        public static bool operator ==(Distance A1, Distance A2)
+        {
+            if (A1.feet != A2.feet)
+                return true;
+            return false;
+        }
+        public override bool Equals(object o)
+        {  
+       return true;  
+         }
+        public override int GetHashCode()
+        {
+            return 0;
+        }
+
+        public static Distance operator +(Distance distance1, Distance distance2)
+        {
+            return new Distance { feet = distance1.feet + distance2.feet, inches = distance1.inches + distance2.inches };
+        }
+        public static Distance operator -(Distance distance1, Distance distance2)
+        {
+            return new Distance { feet = distance1.feet - distance2.feet, inches = distance1.inches - distance2.inches };
+        }
+    }
+}
+        
---
+++ Lab9/Lab11/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Distance distance1 = new Distance();
+            Distance distance2 = new Distance();
+            Distance distminus = new Distance();
+            Distance distplus = new Distance();
+
+            Console.WriteLine("Enter foot 1");
+            distance1.feet = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter foot 2");
+            distance2.feet = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter inches 1");
+            distance1.inches = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter inches 2");
+            distance2.inches = double.Parse(Console.ReadLine());
+
+            
+            distplus = distance1 + distance2;
+            distminus = distance1 - distance2;
+            bool c = distance1 == distance2;
+            Console.WriteLine(c);
+            Console.WriteLine(distplus);
+            Console.WriteLine(distminus);
+            Console.Read();
+
+
+        }
+    }
+}
